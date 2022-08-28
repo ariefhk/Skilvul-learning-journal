ad5c0912da54edae6e6b197a5b304fac07b19508
ariefhk <ariefrachmanhakim1001@gmail.com>
2022-08-29 01:50:56 +0700

    add built in func

---
+++ Python/Basic/6_builtin_function.py
@@ -0,0 +1,20 @@
+# int() => mengubah menjadi int
+x = int('12')
+y = int(12.7)
+
+print(x)
+print(y)
+
+# pow => nilai pangkat
+var_num1 = pow(3,3) #3 pangkat 3
+var_num2 = pow(3,-3) #3 pangkat -3
+
+print(var_num1)
+print(var_num2)
+
+# str() => mengubah menjadi String
+var_str1 = str(12)
+var_str2 = str(12.5)
+
+print(type(var_str1))
+print(type(var_str2))
--- Python/Lanjutan/test.py
+++ Python/Lanjutan/test.py
@@ -1,1 +1,2 @@
 print('test doang')
+print( 30 * 2 + 10 - (21  // 5) )
